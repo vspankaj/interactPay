489747ed8c6a75d065f14f7079780ace18a4c884
vspankajcrm <vishnu.pankaj@crmantra.com>
2022-05-05 16:22:32 +0530

    Ruuning Project 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 const cors = require('cors');
 
-const APP_PORT = process.env.APP_PORT || 8000;
+const APP_PORT = process.env.port || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
